918281ee70cc16b2ff900214ab8f87ee3e57643a
Matthew Iannucci <miannucci@navatekltd.com>
2018-12-03 16:51:02 -0500

    Add 1.2 puzzle solution

---
+++ 1.2/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::string::String;
+use std::collections::HashSet;
+
+fn main() {
+    let mut freq: i64 = 0;
+    let mut found_freqs: HashSet<i64> = HashSet::new();
+
+    let mut raw_frequencies = String::new();
+    File::open("data/input.txt")
+        .expect("Unable to read from input.txt")
+        .read_to_string(&mut raw_frequencies)
+        .expect("Unable to feed contents to string");
+
+    let mut iteration_count = 0;
+    'repeat: loop {
+        println!("Searching... iteration {}", iteration_count);
+
+        'lines: for raw_freq in raw_frequencies.lines() {
+            match &raw_freq[..1] {
+                "+" => freq += &raw_freq[1..].parse::<i64>().unwrap(),
+                "-" => freq -= &raw_freq[1..].parse::<i64>().unwrap(),
+                _ => println!("Parsed unexpected value... ignoring"),
+            };
+
+            if found_freqs.contains(&freq) {
+                println!("Found repeating frequency");
+                break 'repeat;
+            }
+
+            &found_freqs.insert(freq);
+        }
+
+        iteration_count += 1;
+    }
+    println!("Resulting Frequency: {}", freq);
+}
