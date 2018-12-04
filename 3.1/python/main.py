8df5d1f715263a599e873463fac773ab9dc09d9b
Matthew Iannucci <mpiannucci@gmail.com>
2018-12-03 23:12:29 -0500

    Add solution for 3.1

--- 3.1/python/main.py
+++ 3.1/python/main.py
@@ -1,15 +1,16 @@
+import numpy as np
 
 class Rectangle:
-    def __init__(self, claim_id, left, top, width, length):
+    def __init__(self, claim_id, left, top, width, height):
         self.claim_id = claim_id
         self.left = left
         self.top = top
         self.width = width
-        self.length = length
+        self.height = height
 
     @staticmethod
     def from_str(input):
-        params = input.replace('\n').split(' ')
+        params = input.replace('\n', '').split(' ')
         claim_id = params[0].replace('#', '')
         
         coords = params[2].replace(':', '').split(',')
@@ -18,11 +19,19 @@ class Rectangle:
 
         dims = params[3].split('x')
         width = int(dims[0])
-        length = int(dims[1])
+        height = int(dims[1])
 
-        return Rectangle(claim_id, left, top, width, length)
+        return Rectangle(claim_id, left, top, width, height)
 
 rects = []
 with open('./../input.txt', 'r') as f:
     for line in f:
         rects.append(Rectangle.from_str(line))
+
+design_space = np.zeros((1000, 1000))
+for rect in rects:
+    design_space[rect.top:rect.top+rect.height, rect.left:rect.left+rect.width] += 1
+design_space[design_space < 2] = 0
+design_space[design_space > 0] = 1
+
+print('Resulting overlapping square inches: ' + str(design_space.sum()))
