b418bbdeb5053d3dc658cfa61e3f13be7293cd81
Matthew Iannucci <miannucci@navatekltd.com>
2018-12-05 16:38:29 -0500

    Better zig solution

--- 1.1/zig/src/main.zig
+++ 1.1/zig/src/main.zig
@@ -7,34 +7,14 @@ const allocator = std.debug.global_allocator;
 
 pub fn main() anyerror!void {
     const inputFilePath = try os.path.join(allocator, "..", "input.txt");
-
     const rawFreqData = try io.readFileAlloc(allocator, inputFilePath);
 
     var freq: i64 = 0;
 
-    var byteIndex: usize = 0;
-    var freqBuffer = try Buffer.initSize(allocator, 0);
-    defer freqBuffer.deinit();
-    while (byteIndex < rawFreqData.len) {
-        switch (rawFreqData[byteIndex]) {
-            '\r' => {
-                byteIndex += 1;
-            },
-            '\n' => {
-                freq = freq + (try fmt.parseInt(i64, freqBuffer.toSlice(), 10));
-                try freqBuffer.resize(0);
-                byteIndex += 1;
-            },
-            else => {
-                try freqBuffer.appendByte(rawFreqData[byteIndex]);
-                byteIndex += 1;
-            },
-        }
-    }
-
-    if (freqBuffer.len() > 0) {
-        freq = freq + (try fmt.parseInt(i64, freqBuffer.toSlice(), 10));
-        try freqBuffer.resize(0);
+    var lines = std.mem.split(rawFreqData, "\n");
+    while (lines.next()) |line| {
+        const raw_freq = fmt.trim(line);
+        freq += fmt.parseInt(i64, raw_freq, 10) catch 0;
     }
 
     std.debug.warn("Resulting Frequency: {}\n", freq);
