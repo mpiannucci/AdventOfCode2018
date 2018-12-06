7002d0832c14fca874e36f0a16c8269a45103ac9
Matthew Iannucci <miannucci@navatekltd.com>
2018-12-06 13:44:16 -0500

    Add 2.2 zig solution

---
+++ 2.2/zig/src/main.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const mem = std.mem;
+const allocator = std.debug.global_allocator;
+const os = std.os;
+const io = std.io;
+const Buffer = std.Buffer;
+
+pub fn main() anyerror!void {
+    const inputFilePath = try os.path.join(allocator, "..", "input.txt");
+    const rawBoxIds = try io.readFileAlloc(allocator, inputFilePath);
+
+    var commonBoxIdBuffer = Buffer.initNull(allocator);
+    defer commonBoxIdBuffer.deinit();
+
+    var boxIdIter = std.mem.split(rawBoxIds, "\n");
+    outer: while (boxIdIter.next()) |boxId| {
+        var otherBoxIdIter = std.mem.split(rawBoxIds, "\n");
+        while (otherBoxIdIter.next()) |otherBoxId| {
+            try commonBoxIdBuffer.resize(0);
+            var diffCount: u8 = 0;
+            for (boxId) |char, i| {
+                if (boxId[i] != otherBoxId[i]) {
+                    diffCount += 1;
+                } else {
+                    try commonBoxIdBuffer.appendByte(char);
+                }
+            }
+            
+            if (diffCount == 1) {
+                break :outer;
+            }
+        }
+    }
+
+    std.debug.warn("Common Box ID: {}", commonBoxIdBuffer.toOwnedSlice());
+}
